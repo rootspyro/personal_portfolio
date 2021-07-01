6132f2a114eec7a775f06bf90fc2f033e8f4d72a
rootspyro <root.spyro@gmail.com>
2021-07-01 04:21:33 -0400

    eliminando nav.js

--- src/utils/router.js
+++ src/utils/router.js
@@ -1,17 +1,14 @@
-import Nav from '../templates/nav'
 import Footer from '../pages/footer'
 import Routes from './routes'
 
 const Router = async () => {
-	const header = null || document.getElementById('header')
+	//const header = null || document.getElementById('header')
 	const container = null || document.getElementById('main-container')
 	const footer = null || document.getElementById('footer')
 
 	footer.innerHTML = Footer()
 
 	let route = window.location.pathname
-
-	//header.innerHTML = await Nav(route)
 
 	let render = Routes[route] ? Routes[route] : window.location.pathname = '/404' 
 	container.innerHTML = await render
