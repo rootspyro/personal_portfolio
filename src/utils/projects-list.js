0adc993f525e443fa4d9430547da7e2cb08bf8af
rootspyro <root.spyro@gmail.com>
2021-07-09 20:40:27 -0400

    eliminando proyectos de prueba

--- src/utils/projects-list.js
+++ src/utils/projects-list.js
@@ -8,20 +8,6 @@ let projectsList = [
 		description: 'My first personal project. This website is for computer engineering students at my university to learn to code',
 		github: '#' 
 	},
-	{
-		name: 'test',
-		link: '#',
-		img: dcytImg,
-		description: 'this is just an example bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla',
-		github: 'https://github.com/rootspyro?tab=repositories' 
-	},
-	{
-		name: 'test',
-		link: '#',
-		img: dcytImg,
-		description: 'this is just an example bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla',
-		github: 'https://github.com/rootspyro?tab=repositories' 
-	},
 ]
 
 export default projectsList
